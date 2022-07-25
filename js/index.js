7cab783a3e4bec70e2fab93686f2604eeaea9b08
韩洋 <731314840@qq.com>
2022-07-25 19:07:39 +0800

    'create:'柱状图就业行业''

--- js/index.js
+++ js/index.js
@@ -323,3 +323,225 @@
     myChart.setOption(option);
   });
 })();
+// 折线图2模块制作
+(function () {
+  // 1.创建实例对象
+  var myChart = echarts.init(document.querySelector(".Five"));
+  // 2.指定配置和数据
+  option = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "cross",
+        label: {
+          backgroundColor: "#6a7985",
+        },
+      },
+    },
+    legend: {
+      top: "0%",
+      textStyle: {
+        color: "rgba(255,255,255,.5)",
+        fontSize: "12",
+      },
+      data: ["邮件营销", "联盟广告", "Video Ads", "Direct", "Search Engine"],
+    },
+    toolbox: {},
+    grid: {
+      left: "10",
+      top: "30",
+      right: "10",
+      bottom: "10",
+      // containLabel: true,
+    },
+    xAxis: [
+      {
+        type: "category",
+        boundaryGap: false,
+        data: [
+          "01",
+          "02",
+          "03",
+          "04",
+          "05",
+          "06",
+          "07",
+          "08",
+          "09",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14",
+          "15",
+          "16",
+          "17",
+          "18",
+          "19",
+          "20",
+          "21",
+          "22",
+          "23",
+          "24",
+          "25",
+          "26",
+          "26",
+          "28",
+          "29",
+          "30",
+        ],
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12,
+          },
+        },
+        // x轴线的颜色为   rgba(255,255,255,.2)
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.2)",
+          },
+        },
+      },
+    ],
+    yAxis: [
+      {
+        type: "value",
+        axisTick: { show: false },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)",
+          },
+        },
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12,
+          },
+        },
+        // 修改分割线的颜色
+        splitLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)",
+          },
+        },
+      },
+    ],
+    series: [
+      {
+        //第一条 线是圆滑
+        smooth: true,
+        // 单独修改线的样式
+        lineStyle: {
+          color: "#0184d5",
+          width: 2,
+        },
+        // 填充区域
+        areaStyle: {
+          // 渐变色，只需要复制即可
+          color: new echarts.graphic.LinearGradient(
+            0,
+            0,
+            0,
+            1,
+            [
+              {
+                offset: 0,
+                color: "rgba(1, 132, 213, 0.4)", // 渐变色的起始颜色
+              },
+              {
+                offset: 0.8,
+                color: "rgba(1, 132, 213, 0.1)", // 渐变线的结束颜色
+              },
+            ],
+            false
+          ),
+          shadowColor: "rgba(0, 0, 0, 0.1)",
+        },
+        // 设置拐点 小圆点
+        symbol: "circle",
+        // 拐点大小
+        symbolSize: 8,
+        // 设置拐点颜色以及边框
+        itemStyle: {
+          color: "#0184d5",
+          borderColor: "rgba(221, 220, 107, .1)",
+          borderWidth: 12,
+        },
+        // 开始不显示拐点 鼠标经过显示
+        showSymbol: false,
+        name: "邮件营销",
+        type: "line",
+        stack: "Total",
+        areaStyle: {},
+        emphasis: {
+          focus: "series",
+        },
+        // series  第一个对象data数据
+        data: [
+          30, 40, 30, 40, 30, 40, 30, 60, 20, 40, 30, 40, 30, 40, 30, 40, 30,
+          60, 20, 40, 30, 40, 30, 40, 30, 40, 20, 60, 50, 40,
+        ],
+      },
+      {
+        name: "联盟广告",
+        type: "line",
+        smooth: true,
+        stack: "Total",
+        lineStyle: {
+          normal: {
+            color: "#00d887",
+            width: 2,
+          },
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(
+              0,
+              0,
+              0,
+              1,
+              [
+                {
+                  offset: 0,
+                  color: "rgba(0, 216, 135, 0.4)",
+                },
+                {
+                  offset: 0.8,
+                  color: "rgba(0, 216, 135, 0.1)",
+                },
+              ],
+              false
+            ),
+            // 设置拐点 小圆点
+            symbol: "circle",
+            // 拐点大小
+            symbolSize: 5,
+            // 设置拐点颜色以及边框
+            itemStyle: {
+              color: "#00d887",
+              borderColor: "rgba(221, 220, 107, .1)",
+              borderWidth: 12,
+            },
+            showSymbol: false,
+            // shadowColor: "rgba(0, 0, 0, 0.1)",
+          },
+        },
+        emphasis: {
+          focus: "series",
+        },
+        // series  第二个对象data数据
+        data: [
+          130, 10, 20, 40, 30, 40, 80, 60, 20, 40, 90, 40, 20, 140, 30, 40, 130,
+          20, 20, 40, 80, 70, 30, 40, 30, 120, 20, 99, 50, 20,
+        ],
+      },
+    ],
+  };
+  // 3. 把配置给实例对象
+  myChart.setOption(option);
+  // 4.让图表跟随屏幕自动去适应
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+})();
